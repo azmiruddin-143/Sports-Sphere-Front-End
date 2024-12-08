8dad8fff83e0be5a65f17ed8351d45416a58e946
rjazmirkhan <bangladeshfreelancerazmir@gmail.com>
2024-12-08 08:35:52 +0600

    latest 6 data show

--- src/Componenets/HomePages/Products.jsx
+++ src/Componenets/HomePages/Products.jsx
@@ -6,6 +6,9 @@ const Products = () => {
     const productLoader = useLoaderData()
     console.log(productLoader);
     return (
+         
+        // latest 6 data show//
+
         <div className='sm:my-16 my-5'>
             <Slide direction="up" duration={2000} triggerOnce>
                 <div className='grid sm:grid-cols-2  md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 place-items-center sm:gap-y-5 lg:gap-y-12 lg:mx-5 2xl:mx-32 xl:mx-24'>
