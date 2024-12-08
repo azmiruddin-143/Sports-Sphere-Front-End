11feb9d063d13b37ef6fe32515f40b2fafce5adb
rjazmirkhan <bangladeshfreelancerazmir@gmail.com>
2024-12-08 15:50:04 +0600

    Redmi file add

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
-# React + Vite
+# Assignment Category : Lotus
+# Website Name : Sports Sphere
+# Website Link : 
+# Features of Sports Website
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- Dynamic Display
+- Theme Toggle
+- Real-Time Balance
+- Responsive Design
+- Smooth Interactions
+- Category Showcase
+- Dark Mode Support
+- Live Updates
+- Interactive Cards
+- Engaging UI
--- src/FirebaseSetup/Firebase.init.js
+++ src/FirebaseSetup/Firebase.init.js
@@ -5,13 +5,22 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCgfvTu9K1Fv5ijFv2rHqSM6UV4v8W7r8w",
+//   authDomain: "sports-equipment-store-2ec4b.firebaseapp.com",
+//   projectId: "sports-equipment-store-2ec4b",
+//   storageBucket: "sports-equipment-store-2ec4b.firebasestorage.app",
+//   messagingSenderId: "842266911171",
+//   appId: "1:842266911171:web:897c64f30d2ab8e8481d76"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCgfvTu9K1Fv5ijFv2rHqSM6UV4v8W7r8w",
-  authDomain: "sports-equipment-store-2ec4b.firebaseapp.com",
-  projectId: "sports-equipment-store-2ec4b",
-  storageBucket: "sports-equipment-store-2ec4b.firebasestorage.app",
-  messagingSenderId: "842266911171",
-  appId: "1:842266911171:web:897c64f30d2ab8e8481d76"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId:import.meta.env. VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase setup complete//
